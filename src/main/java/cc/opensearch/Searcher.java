ad3b65981bccf8a08ab28b66e80aab0a2efc0c9c
David Urbansky <davidurbansky@gmail.com>
2023-12-22 15:59:03 +0100

    cache html templates per API response

--- src/main/java/cc/opensearch/Searcher.java
+++ src/main/java/cc/opensearch/Searcher.java
@@ -2,6 +2,8 @@ package cc.opensearch;
 
 import org.apache.log4j.Logger;
 import ws.palladian.helper.ConfigHolder;
+import ws.palladian.helper.UrlHelper;
+import ws.palladian.helper.collection.MapBuilder;
 import ws.palladian.helper.io.FileHelper;
 import ws.palladian.helper.nlp.StringHelper;
 import ws.palladian.persistence.json.JsonArray;
@@ -21,6 +23,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Searcher {
     private static final Logger LOGGER = Logger.getLogger(Searcher.class);
     private final JsonDatabase jsonDatabase;
+    private static final String RESPONSES_COLLECTION = "responses";
+    private static final String TEMPLATES_COLLECTION = "templates";
     private final OpenAiApi openAiApi;
     private final String apiDescriptions;
 
@@ -39,7 +43,8 @@ public class Searcher {
         openAiApi = new OpenAiApi(ConfigHolder.getInstance().getConfig());
 
         jsonDatabase = new JsonDatabase("data", 1000);
-        jsonDatabase.createIndex("responses", "_id");
+        jsonDatabase.createIndex(RESPONSES_COLLECTION, "_id");
+        jsonDatabase.createIndex(TEMPLATES_COLLECTION, "source");
 
         ClassLoader classLoader = getClass().getClassLoader();
         apiDescriptions = FileHelper.readFileToString(classLoader.getResourceAsStream("apis.txt"));
@@ -47,15 +52,24 @@ public class Searcher {
         htmlRenderingPrompt = FileHelper.readFileToString(classLoader.getResourceAsStream("html-render-prompt.txt"));
     }
 
+    /**
+     * Some APIs require authentication. If we don't have any authentication information, we filter out those APIs.
+     */
+    private void filterApisIfNoAuthenticationAvailable() {
+        // FIXME
+    }
+
+
     public JsonObject search(String query) throws Exception {
         String jsKey = StringHelper.makeSafeName(query);
 
         // see whether we have a response for the query already
-        JsonObject apiResponse = jsonDatabase.getOne("responses", "_id", jsKey);
+        JsonObject apiResponse = jsonDatabase.getOne(RESPONSES_COLLECTION, "_id", jsKey);
         if (apiResponse != null) {
             LOGGER.info("found response in database");
             return apiResponse;
         }
+        apiResponse = new JsonObject();
 
         JsonArray messages = new JsonArray();
 
@@ -69,13 +83,35 @@ public class Searcher {
         userMessage.put("content", apiUsagePrompt.replace("#QUERY#", query));
         messages.add(userMessage);
 
-        String response = openAiApi.chat(messages, 0., new AtomicInteger(), "gpt-4-1106-preview", 4095);
-        //        String response = openAiApi.chat(messages, 0., new AtomicInteger());
+        String apiUrl = openAiApi.chat(messages, 0., new AtomicInteger(), "gpt-4-1106-preview", 4095);
 
-        apiResponse = new DocumentRetriever().tryGetJsonObject(response);
+        if (apiUrl == null || !apiUrl.startsWith("http")) {
+            LOGGER.error("AI response not a URL: " + apiUrl);
+            return null;
+        }
+
+        DocumentRetriever documentRetriever = new DocumentRetriever();
+        documentRetriever.setGlobalHeaders(MapBuilder.createPut("Accept", "application/json").create());
+        String textResponse = documentRetriever.getText(apiUrl);
+        if (textResponse == null) {
+            LOGGER.error("API response not valid: " + textResponse);
+            return null;
+        } else {
+            if (textResponse.startsWith("[")) {
+                JsonArray array = JsonArray.tryParse(textResponse);
+                apiResponse.put("response", array);
+            } else {
+                apiResponse = JsonObject.tryParse(textResponse);
+            }
+        }
+        if (apiResponse == null) {
+            LOGGER.error("API response not valid: " + textResponse);
+            return null;
+        }
         apiResponse.put("_id", jsKey);
+        apiResponse.put("source", apiUrl.replaceAll("\\?.*", ""));
 
-        jsonDatabase.add("responses", apiResponse);
+        jsonDatabase.add(RESPONSES_COLLECTION, apiResponse);
 
         LOGGER.info("open ai returned: " + apiResponse);
 
@@ -83,24 +119,39 @@ public class Searcher {
     }
 
     public String renderHtml(JsonObject apiResponse) throws Exception {
-        JsonArray messages = new JsonArray();
+        if (apiResponse == null) {
+            return null;
+        }
+        JsonObject templateJson = jsonDatabase.getOne(TEMPLATES_COLLECTION, "source", apiResponse.tryGetString("source"));
 
-        JsonObject systemMessage = new JsonObject();
-        systemMessage.put("role", "system");
-        systemMessage.put("content", "We have the following API response:\n\n" + apiResponse.toString());
-        messages.add(systemMessage);
+        String htmlResponse;
+        if (templateJson != null) {
+            htmlResponse = templateJson.tryGetString("html");
+        } else {
+            JsonArray messages = new JsonArray();
 
-        JsonObject userMessage = new JsonObject();
-        userMessage.put("role", "user");
-        userMessage.put("content", htmlRenderingPrompt);
-        messages.add(userMessage);
+            JsonObject systemMessage = new JsonObject();
+            systemMessage.put("role", "system");
+            systemMessage.put("content", "We have the following API response:\n\n" + apiResponse);
+            messages.add(systemMessage);
 
-        String response = openAiApi.chat(messages, 0., new AtomicInteger(), "gpt-4-1106-preview", 4095);
+            JsonObject userMessage = new JsonObject();
+            userMessage.put("role", "user");
+            userMessage.put("content", htmlRenderingPrompt);
+            messages.add(userMessage);
+
+            htmlResponse = openAiApi.chat(messages, 0., new AtomicInteger(), "gpt-4-1106-preview", 4095);
+
+            JsonObject template = new JsonObject();
+            template.put("html", htmlResponse);
+            template.put("source", apiResponse.tryGetString("source"));
+            jsonDatabase.add(TEMPLATES_COLLECTION, template);
+        }
 
         long responseId = System.currentTimeMillis();
         String responseJsonFileName = "response" + responseId + ".json";
         FileHelper.writeToFile("data/"+responseJsonFileName, apiResponse.toString());
-        String html = StringHelper.getSubstringBetween(response, "```html", "```");
+        String html = StringHelper.getSubstringBetween(htmlResponse, "```html", "```");
         html = html.replace("'response.json'", "'responses/" + responseId + "'");
 
         LOGGER.info("open ai returned HTML: " + html);
