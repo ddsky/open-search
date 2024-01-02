12520a214eccdcd569f1822c2c91524ae24841ce
David Urbansky <davidurbansky@gmail.com>
2024-01-02 13:47:02 +0100

    ability to use together or openai

---
+++ src/main/java/cc/opensearch/LargeLanguageModelApi.java
@@ -0,0 +1,57 @@
+package cc.opensearch;
+
+import org.apache.commons.configuration.Configuration;
+import ws.palladian.helper.ConfigHolder;
+import ws.palladian.persistence.json.JsonArray;
+import ws.palladian.retrieval.search.web.OpenAiApi;
+import ws.palladian.retrieval.search.web.TogetherApi;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * This class wraps large language model APIs: OpenAI and Together AI for open source models.
+ *
+ * @author David Urbansky
+ * @since 02.01.2024 at 11:16
+ **/
+public class LargeLanguageModelApi {
+    /** Open AI */
+    private final OpenAiApi openAiApi;
+
+    /** Together AI to use open source models */
+    private final TogetherApi togetherApi;
+
+    static class SingletonHolder {
+
+        static LargeLanguageModelApi instance = new LargeLanguageModelApi();
+    }
+
+    public static LargeLanguageModelApi getInstance() {
+        return LargeLanguageModelApi.SingletonHolder.instance;
+    }
+
+    private LargeLanguageModelApi() {
+        // check for which API we have an API key
+        Configuration config = ConfigHolder.getInstance().getConfig();
+        if (config.containsKey(OpenAiApi.CONFIG_API_KEY)) {
+            openAiApi = new OpenAiApi(config);
+        } else {
+            openAiApi = null;
+        }
+        if (config.containsKey(TogetherApi.CONFIG_API_KEY)) {
+            togetherApi = new TogetherApi(config);
+        } else {
+            togetherApi = null;
+        }
+    }
+
+    public String chat(JsonArray messages, double temperature, AtomicInteger usedTokens, String modelName, int maxTokens) throws Exception {
+        if (openAiApi != null) {
+            return openAiApi.chat(messages, temperature, usedTokens, modelName, maxTokens);
+        }
+        if (togetherApi != null) {
+            return togetherApi.chat(messages, temperature, usedTokens, modelName, maxTokens);
+        }
+        return null;
+    }
+}
